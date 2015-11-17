c4230cd75b02c115600ecba04c08c6b9fc3ce5ad
Casey Kolderup <casey@kolderup.org>
2015-11-16 21:57:29 -0500

    pivot to amiibo_oracle

--- nintycal.rb
+++ nintycal.rb
@@ -1,16 +1,22 @@
 require 'twitter'
 
 templates = [
-    "My uncle works at Nintendo and he said %d is the year of %s.",
-    "OPINION: is %d the Year of %s?",
-    "%d is the Year of %s! #nintendolife #gameon #playitloud",
-    "RUMOR: %d to be declared the Year of %s by Nintendo",
-    "I'm hearing rumblings that Nintendo will announce %d as the Year of %s...",
-    "Did you hear? #Nintendo wants to make %d the Year of %s!",
-    "Dear Nintendo, PLEASE make %d the Year of %s!!!!!",
-    "haha what if Nintendo made %d the year of %s?",
-    "ok seriously tho if Nintendo doesn't make %d the year of %s I'll die",
-    "I'll buy a Nintendo console in %d if Nintendo makes it the Year of %s",
+    "BREAKING NEWS: New Amiibos confirmed: %s!!!",
+    "etika's latest vid says next amiibo wave has %s? nfw lol",
+    "New Amiibo wave rumored to include %s, Nintendo Direct scheduled for tomorrow",
+    "TARGET EXCLUSIVE CADMIUM-PLATED AMIIBOS!!! New Characters include %s",
+    "Hearing rumblings that the next announced wave of Amiibos will have %s. Whoa.",
+    "Not sure if I'm translating this right but according to 2ch %s are Japan-only Amiibo????",
+    "Nintendo KNOWS me: I can't wait to pick up %s from the next Amiibo wave",
+    "Exciting news!!!! %s are the next Amiibos coming out!!!!",
+    "I never thought I'd see the day when %s got made into Amiibos... so hype that they're next",
+    "yes yes yes MORE AMIIBO: %s IN THE NEXT WAVE!",
+    "Thought you'd be waiting forever for %s Amiibos? Think again",
+    "I just hope Nintendo realizes they NEED to make 10x as many %s amiibos to meet demand",
+    "I'm gonna cry if I can't get ahold of %s from the new amiibo wave",
+    "I LOVE AMIIBO and I am soooo excited for %s",
+    "They can probably stop making new Amiibo once %s come out",
+    "CANNOT. BELIEVE. IT. %s Amiibo? THANK YOU NINTENDO!!!!!"
 ]
 
 def character
